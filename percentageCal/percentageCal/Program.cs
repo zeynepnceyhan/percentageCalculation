fc95810c306daf0971bcec2b15784bf551e548aa
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 16:05:07 +0300

    percentage calculation
    internship

---
+++ percentageCal/percentageCal/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number, per, result;
+        Console.WriteLine("Please give me a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter a percentage: ");
+        per = Convert.ToInt32(Console.ReadLine());
+
+        result = (number * per) / 100;
+
+        Console.WriteLine("The x% of our number: " + result);
+
+        Console.ReadLine();
+    }
+
+}
+
